a736de4a1fc382a1f80e8e319ad6500bfad59ed4
Alex Aguirre <teamaguirre@gmail.com>
2022-08-05 19:43:19 -0700

    Adding 404 page default route

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express()
 require('dotenv').config()
 
 app.get('/',(req,res)=>{
-    res.send('Hello World')
+    res.send('Hello My Fellow FSDs')
+})
+app.get('*',(req,res)=>{
+    res.status(404).send(`<h1>404 Page not found</h1>`)
+
 })
 
 app.listen(process.env.PORT)

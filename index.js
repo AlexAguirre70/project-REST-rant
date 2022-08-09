4b79ebc572b5a01a7e12e75258773e3981bcf7fd
Alex Aguirre <teamaguirre@gmail.com>
2022-08-08 19:12:46 -0700

    Adding Home view template file

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express= require('express')
+const home = require('./views/home')
 const app = express()
 
+//middleware set views engine
+app.set('view engine','jsx')
+app.engine('jsx',require('express-react-views').createEngine())
+
+//environment variables
 require('dotenv').config()
 
 app.use('/places',require('./controllers/places'))
 
 app.get('/',(req,res)=>{
-    res.send('Hello My Fellow FSDs')
+    res.render('home')
 })
 app.get('*',(req,res)=>{
     res.status(404).send(`<h1>404 Page not found</h1>`)
